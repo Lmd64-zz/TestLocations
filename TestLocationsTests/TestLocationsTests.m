15bd8129e0cc7824e311f31e02e32f4d0b819208
Liam Dunne <liamodunne@gmail.com>
2013-10-28 21:20:26 +0000

    Initial Commit

---
+++ TestLocationsTests/TestLocationsTests.m
@@ -0,0 +1,34 @@
+//
+//  TestLocationsTests.m
+//  TestLocationsTests
+//
+//  Created by Liam Dunne on 28/10/2013.
+//  Copyright (c) 2013 Liam Dunne. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestLocationsTests : XCTestCase
+
+@end
+
+@implementation TestLocationsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
